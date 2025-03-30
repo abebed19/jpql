5558a872190f676484775ca3306a37d9318df3b8
abebed19 <30549320+abebed19@users.noreply.github.com>
2025-03-30 20:35:28 +0300

    More details jpql examples

--- src/main/java/com/abbayllc/Main.java
+++ src/main/java/com/abbayllc/Main.java
@@ -8,9 +8,11 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.TypedQuery;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -31,16 +33,11 @@ public class Main {
 
             manager.getTransaction().begin();
 
-            //SELECT, UPDATE, DELETE we can perform these querys in out jpql but not insert
+            /*SELECT, UPDATE, DELETE we can perform these querys in out jpql but not insert
 
-            //String jpql ="SElECT p FROM Product p where p.price >1500";
-            // we can extend the above query even to be paramterized like as follows
-            String jpql = "select p from Product  p where p.price > :price and  p.name LIKE :name";
+            String jpql ="SElECT p FROM Product p where p.price >1500";
+             we can extend the above query even to be paramterized like as follows
 
-
-            TypedQuery<Product>  query =  manager.createQuery(jpql, Product.class);
-            query.setParameter("price",1500);
-            query.setParameter("name","%m%");
 
 
             List<Product> products  = query.getResultList();
@@ -48,6 +45,44 @@ public class Main {
             for( Product p : products){
                 System.out.println(p);
             }
+
+             */
+//            String jpql = "select p from Product  p where p.price > :price and  p.name LIKE :name";
+//
+//
+//            TypedQuery<Product>  query =  manager.createQuery(jpql, Product.class);
+//            query.setParameter("price",1500);
+//            query.setParameter("name","%M%");
+
+// finding average value
+//            String jpql = "select avg(p.price) from Product p";
+//            TypedQuery<Double> query = manager.createQuery(jpql,Double.class);
+//            Double averagePrice = query.getSingleResult();
+//
+//            System.out.println("Average price : " + averagePrice);
+
+//
+//            String jpqlCount = "Select Count(p) from Product p";
+//
+//            TypedQuery<Long> count = manager.createQuery(jpqlCount,Long.class);
+//
+//            Long numofProducts = count.getSingleResult();
+//
+//            System.out.println(numofProducts);
+
+
+            //selecting specific attribute
+            String jpql3 = """
+                           select  p.name,avg(p.price) 
+                           from Product p group by p.name
+                           """;
+            TypedQuery<Object[]> q  =manager.createQuery(jpql3, Object[].class);
+
+            q.getResultList().forEach(objects -> {
+                System.out.println(objects[0] +" " + objects[1] );
+            });
+
+
 
 
             manager.getTransaction().commit();
